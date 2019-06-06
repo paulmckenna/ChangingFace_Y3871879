9017ebedc0b2402da91406dcb5ea52bb6f41be0c
paulmckenna <paulmckenna@10.59.125.210>
2019-06-06 21:58:52 +0100

    changed left eye colour

--- Assignment Three/MyJavaFX/src/ChangingFace1_Y3871879.java
+++ Assignment Three/MyJavaFX/src/ChangingFace1_Y3871879.java
@@ -32,7 +32,7 @@ public class ChangingFace1_Y3871879 extends Application {
 		 // create and configure the circle for the left eye         
 		 Circle leftEye = new Circle(162, 100, 10);         
 		 leftEye.setFill(Color.RED);         
-		 leftEye.setStroke(Color.BLUE);   
+		 leftEye.setStroke(Color.GREEN);   
 		 
 		 // create and configure a smiling mouth (this is how it will start)    
 		 Arc mouth = new Arc(125, 150, 45, 35, 0,-180);          
